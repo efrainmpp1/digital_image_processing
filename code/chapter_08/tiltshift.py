6cc21c305d03d64c0e1abefa68867b995feddee3
efrainmpp1 <efrainmpp@hotmail.com>
2023-07-17 12:27:52 -0300

    fix code

--- code/chapter_08/tiltshift.py
+++ code/chapter_08/tiltshift.py
@@ -1,45 +1,115 @@
 import cv2
 import numpy as np
 
-def apply_tilt_shift(image, region_height, decay, center_position):
-    # Converter a imagem para escala de cinza
-    image_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+class TiltShift:
+    __slider_max = 100
+    __track_bars = False
+    __d_window_name = "forca decaimento"
+    __l1_window_name = "altura"
+    __l2_window_name = "centro"
+    __window_name = "Tilt-Shift"
+    __original_image: np.ndarray = None
+    __blurred_image: np.ndarray = None
+    __ones: np.ndarray = None
+    tilt_shift: np.ndarray = None
+    __delay = -1
 
-    # Calcular o tamanho da região superior e inferior com base na altura da região central
-    height, width = image.shape[:2]
-    top_region_height = int(center_position - region_height / 2)
-    bottom_region_height = int(center_position + region_height / 2)
+    def __init__(self, l1=30, l2=60, d=30):
+        self.__d = d
+        self.__l1 = l1
+        self.__l2_len = l2
+        self.__mean_kernel = np.ones((9, 9), np.float32) / 81
 
-    # Aplicar o desfoque na região superior
-    blurred_top = cv2.GaussianBlur(image_gray[:top_region_height, :], (0, 0), sigmaX=decay)
+    def __create_track_bars(self, window_name: str):
+        try:
+            cv2.getWindowProperty(window_name, 0)
+        except:
+            cv2.namedWindow(window_name)
 
-    # Aplicar o desfoque na região inferior
-    blurred_bottom = cv2.GaussianBlur(image_gray[bottom_region_height:, :], (0, 0), sigmaX=decay)
+        self.__window_name = window_name
+        cv2.createTrackbar(self.__d_window_name, window_name, self.__d, self.__slider_max, self.__change)
+        cv2.createTrackbar(self.__l1_window_name, window_name, self.__l1, self.__slider_max, self.__change)
+        cv2.createTrackbar(self.__l2_window_name, window_name, self.__l2_len, self.__slider_max, self.__change)
+        self.__track_bars = True
 
-    # Combinar as regiões borradas e a região central nítida
-    result = np.vstack((blurred_top, image_gray[top_region_height:bottom_region_height, :], blurred_bottom))
+    def __alpha_f(self, x) -> float:
+        l2 = self.__l1 + self.__l2_len
+        return 0.5 * (np.tanh((x - self.__l1) / self.__d) - np.tanh((x - l2) / self.__d))
 
-    # Converter a imagem resultante para colorida
-    result = cv2.cvtColor(result, cv2.COLOR_GRAY2BGR)
+    def __change(self, value: int):
+        self.__update_parameters()
 
-    # Retornar o resultado
-    return result
+        original_weight = np.zeros(self.__original_image.shape)
+        blurred_weight = np.zeros(self.__blurred_image.shape)
 
-# Carregar a imagem
-image = cv2.imread("image.jpg")
+        for x in range(int(original_weight.shape[0])):
+            alpha = self.__alpha_f(x)
 
-# Definir os parâmetros iniciais
-region_height = 200  # Altura da região central que entrará em foco
-decay = 10  # Força de decaimento da região borrada
-center_position = 300  # Posição vertical do centro da região que entrará em foco
+            blurred_weight[x, :] += 1 - alpha
+            original_weight[x, :, :] += alpha
 
-# Aplicar o tilt shift na imagem
-result = apply_tilt_shift(image, region_height, decay, center_position)
+        tilt = np.multiply(original_weight, self.__original_image) / 255
+        shift = np.multiply(blurred_weight, self.__blurred_image) / 255
 
-# Exibir a imagem resultante
-cv2.imshow("Tilt Shift", result)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+        self.tilt_shift = tilt + shift
+        if self.__delay > -1:
+            cv2.imshow(self.__window_name, self.tilt_shift)
 
-# Salvar a imagem resultante em arquivo
-cv2.imwrite("result.jpg", result)
+    @staticmethod
+    def __normalize(x: int, t: int) -> float:
+        return x * t / 100
+
+    def __update_parameters(self):
+        if self.__track_bars:
+            self.__l1 = cv2.getTrackbarPos(self.__l1_window_name, self.__window_name) - 1
+            self.__l2_len = cv2.getTrackbarPos(self.__l2_window_name, self.__window_name)
+            self.__d = 0.5 * cv2.getTrackbarPos(self.__d_window_name, self.__window_name) + 1
+            self.__l1 = self.__normalize(self.__l1, self.__original_image.shape[0])
+            self.__l2_len = self.__normalize(self.__l2_len, self.__original_image.shape[1] / 2)
+
+    def show_track_bars(self, window_name: str):
+        if not self.__track_bars:
+            self.__create_track_bars(window_name)
+
+    def show_tilt_shift(self, delay: int):
+        self.__delay = delay
+        self.__change(0)
+
+        if delay > -1:
+            cv2.imshow("original image", self.__original_image)
+            cv2.waitKey(delay)
+
+    def __blurring_image(self):
+        self.__blurred_image = cv2.filter2D(self.__original_image, cv2.CV_32F, self.__mean_kernel)
+
+        for i in range(5):
+            self.__blurred_image = cv2.filter2D(self.__blurred_image, cv2.CV_32F, self.__mean_kernel)
+
+    def load_image(self, image_path: str):
+        self.__original_image = cv2.imread(image_path)
+        if self.__original_image is None:
+            print("Imagem não encontrada !!")
+            exit(1)
+        self.__blurring_image()
+
+    def set_image(self, image: np.ndarray):
+        self.__original_image = image
+        self.__blurring_image()
+
+    def test_function_alpha(self):
+        from matplotlib import pyplot
+
+        x = np.array([i for i in range(129)])
+        y = self.__alpha_f(x)
+        pyplot.plot(x, 1 - y)
+        pyplot.show()
+
+    def save_image(self, path: str):
+        cv2.imwrite(path, cv2.UMat(self.tilt_shift * 255))
+
+if __name__ == '__main__':
+    tilt_shift = TiltShift()
+    tilt_shift.load_image("./valorant.png")
+    tilt_shift.show_track_bars("Tilt-Shift")
+    tilt_shift.show_tilt_shift(0)
+    tilt_shift.save_image("tiltShift_valorant.png")
